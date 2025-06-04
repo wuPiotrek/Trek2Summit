c36521fedfd754ec912d3040265d4a2b2e277761
Piotr Wójtowicz <44615713+wuPiotrek@users.noreply.github.com>
2025-06-04 10:08:54 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-PiotrW"
+    storage_account_name = "sapiotrw"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "piotrw-app-service-plan"
+  location            = "westeurope"
+  resource_group_name = "rg-PiotrW"
+  os_type             = "Linux"
+  sku_name            = "P0v3"
+}
+
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "piotrw-webapp-t2s-workshop-1"
+  location            = "westeurope"
+  resource_group_name = "rg-PiotrW"
+  service_plan_id     = azurerm_service_plan.example.id
+  site_config {}
+}
